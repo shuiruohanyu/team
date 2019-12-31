8161d4f5eead93f70f50ee247647b524af848f10
gaoly <shuiruohanyu@foxmail.com>
2019-12-31 10:22:52 +0800

    init

---
+++ src/module-hmmm/pages/questions.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="question-container">{{provinces}}{{citys}}</div>
+</template>
+
+<script>
+import {
+  list as questionList,
+  remove as questionRemove
+} from "../../api/hmmm/questions";
+import { list as subjectList } from "../../api/hmmm/subjects";
+import { questionType, difficulty } from "../../api/hmmm/constants";
+import { provinces, citys } from "../../api/hmmm/citys";
+export default {
+  data() {
+    return {
+      list: [],
+      subjects: [],
+      questionType,
+      difficulty,
+      provinces: provinces(),
+      citys: citys("北京市")
+    };
+  },
+  methods: {
+    async getQuestion() {
+      let result = await questionList();
+      this.list = result.data;
+    },
+    async getSubject() {
+      let result = await subjectList();
+      this.subjects = result.data;
+    }
+  },
+  created() {
+    this.getQuestion();
+    this.getSubject();
+  }
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/module-hmmm/pages/tags.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-card>
+    <el-form label-width="100px" inline>
+      <el-form-item label="学科">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="难度">
+        <el-input></el-input>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "TagsList",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+</style>
